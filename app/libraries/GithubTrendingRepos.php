d4b1f4d83882616429d27a61807ef173933214e1
med amine <mohamed.amine.elattabi@gmail.com>
2020-01-24 00:38:33 +0100

    Add code logic to get data from the endpoint

--- app/libraries/GithubTrendingRepos.php
+++ app/libraries/GithubTrendingRepos.php
@@ -4,7 +4,41 @@
 namespace App\libraries;
 
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
 class GithubTrendingRepos
 {
+    /**
+     * @var string
+     */
+    private $endPoint="https://maxday.github.io/trending/data.json";
 
+    /**
+     * @var Client
+     */
+    private $guzzleClient;
+
+
+    public function __construct()
+    {
+        //init guzzle client
+        $this->guzzleClient = new Client();
+    }
+
+    /**
+     * Get data and refactor it
+     * @return bool|mixed
+     */
+    public function getTrendingLanguages()
+    {
+        try {
+            $response = $this->guzzleClient->get($this->end_point);
+            $languages = \json_decode($response->getBody(), TRUE);
+            return $languages;
+
+        } catch (ClientException $ex) {
+            return FALSE;
+        }
+    }
 }
