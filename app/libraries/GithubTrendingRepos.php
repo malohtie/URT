d3d29d6bb3b158b343600cb72b3367c62c00f5a7
med amine <mohamed.amine.elattabi@gmail.com>
2020-01-24 00:40:46 +0100

    refactor the return response to correspond the quiz need

--- app/libraries/GithubTrendingRepos.php
+++ app/libraries/GithubTrendingRepos.php
@@ -35,10 +35,30 @@ class GithubTrendingRepos
         try {
             $response = $this->guzzleClient->get($this->end_point);
             $languages = \json_decode($response->getBody(), TRUE);
-            return $languages;
+            return $this->refactorResponse($languages);
 
         } catch (ClientException $ex) {
             return FALSE;
         }
+    }
+
+    /**
+     * @param $languages
+     * @return bool|mixed
+     */
+    private function refactorResponse($languages)
+    {
+        if(!empty($languages)) {
+            $response['last_scan'] = $languages['extraData']['createdAt'];
+            foreach ($languages['data'] as $key => $value) {
+                $response['languages'][] = [
+                    'language' => $key == "null" ? "others" : $key, //null mean it not a progamming language
+                    'number_repos' => count($languages['data'][$key]),
+                    'repos' => $languages['data'][$key],
+                ];
+            }
+            return $response;
+        }
+        return FALSE;
     }
 }
